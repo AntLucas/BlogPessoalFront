cbe213fe89d3c9de62a8d35a2f6be42393e33fba
Antlucas <antonio.lucas278@outlook.com>
2021-07-12 10:09:00 -0300

    Order by

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -26,6 +26,9 @@ export class InicioComponent implements OnInit {
   user: User = new User()
   idUser: number = environment.idUsuario
 
+  key = 'data'
+  reverse = true
+
   constructor(
     private router: Router,
     private postagemService: PostagemService,
